79010af94f7a58c5d696bf584661632de59a074f
Paulo Roberto <evans_sp@hotmail.com>
2026-05-14 16:22:38 -0300

    Fix: Ajustes no app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('imobiliaria');
+  protected readonly title = signal('landing');
 }
